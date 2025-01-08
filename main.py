61cdc4ef12e6648cb7acd6759026c488b7e2e2af
pavellunin98 <pavel.lunin769@gmail.com>
2025-01-08 13:16:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-completed_tasks = 12
-humber_of_hours = 1.5
-course_name = 'Python'
-time_for_one_task = completed_tasks/humber_of_hours
-print(completed_tasks)
-print(humber_of_hours)
-print(course_name)
-print(time_for_one_task)
+example = ('Pavel')
+print(example[0])
+print(example[-1])
+print(example[2:])
+print(example[::-1])
+print(example[1:4:2])
